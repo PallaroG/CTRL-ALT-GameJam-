fe9e7544de5e378abd97bae96472ed4e455f0726
Gabriel Alexandre Rodrigues Silva <gabrielalecab1@gmail.com>
2026-02-15 15:57:09 -0300

    modelos

---
+++ Assets/_Project/Scripts/ForGameplay/CharacterPositionScript.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CharacterPositionScript : MonoBehaviour
+{
+    /*
+        script para controlar a posição dos personagens na fila.
+
+    */
+}
---
+++ Assets/_Project/Scripts/ForGameplay/contratacaoScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class contratacaoScript : MonoBehaviour
+{
+    /*
+        script para controlar os botões de contratação
+        -----------------------------------------------
+        verifica qual o personagem é o primeiro na fila
+        método que contrata o personagem
+        método que dispensa o personagem
+
+    */
+
+
+    public void contratado()
+    {
+        Debug.Log("Opa vem pra ca");
+    }
+
+    public void dispensado()
+    {
+        Debug.Log("Sai pra lá");
+    }
+
+
+}
