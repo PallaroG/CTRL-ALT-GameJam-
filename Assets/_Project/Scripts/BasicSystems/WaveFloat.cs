ef1dbe4997976dc593f0a9cd6aa465e524c1a2b6
Gabriel Alexandre Rodrigues Silva <gabrielalecab1@gmail.com>
2026-02-14 21:09:41 -0300

    efeito wave e cachorro caramelho

---
+++ Assets/_Project/Scripts/BasicSystems/WaveFloat.cs
@@ -0,0 +1,26 @@
+using SuperAnimatedDialogue.Runtime;
+using UnityEngine;
+
+
+namespace BasicSystems.Effects
+{
+    public class WaveFloat : MonoBehaviour
+    {
+        public float amplitude = 0.5f;
+        public float frequency = 1f;
+
+        private Vector3 startPos;
+
+        void Start()
+        {
+            startPos = transform.position;
+        }
+
+        void Update()
+        {
+            float offset = Mathf.Sin(Time.time * frequency) * amplitude;
+            transform.position = startPos + Vector3.up * offset;
+        }
+    }
+    
+}
